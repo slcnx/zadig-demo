3ba11e71a1a82afa78930e4573b441d28133ee67
slcnx <35824750+slcnx@users.noreply.github.com>
2021-10-26 09:34:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'Web App with Python Flask!'
+def hello_world():
+    return 'Flask: Hello World from Docker'
 
-app.run(host='0.0.0.0', port=3000)
+@app.route('/api')
+def rest_hello_world():
+    return '{"id":1,"message":"Flask: Hello World from Docker"}'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0',  port=3000)
